34ddfc44ddafe912af8fbcdbb534a0478d57900a
Femi Olowonefa <feminefa@gmail.com>
2024-06-18 15:28:23 -0400

    fixed bug with uncomitted file

--- src/components/layouts/main/footer/index.tsx
+++ src/components/layouts/main/footer/index.tsx
@@ -11,6 +11,7 @@ import {
   BsReddit,
   BsTelegram,
   BsYoutube,
+  BsTwitterX,
 } from 'react-icons/bs';
 
 interface AppFooterProps {}
@@ -19,13 +20,7 @@ export const AppFooter = (props: AppFooterProps) => {
     <footer className="p-4 lg:p-12 mt-10 bg-gray-300 dark:bg-[#292929] flex flex-col gap-12   ">
       <div className="flex ml-auto">
         <div className="flex  gap-4">
-        <Link
-                className="inline-block hover:text-white transition-all duration-300"
-                target='_blank'
-                href="https://github.com/mlayerprotocol"
-              >
-                <BsGithub />
-              </Link>
+       
               {/* <Link
                 className="inline-block hover:text-white transition-all duration-300"
                 href="#"
@@ -37,55 +32,99 @@ export const AppFooter = (props: AppFooterProps) => {
                 href="https://twitter.com/mlayerprotocol"
                 target='_blank'
               >
-                <BsX />
+                <BsTwitterX size={30}  />
               </Link>
               <Link
                 className="inline-block hover:text-white transition-all duration-300"
                 href="https://discord.gg/QazYZYBqus"
                 target='_blank'
               >
-                <BsDiscord />
+                <BsDiscord size={30}  />
+          </Link>
+          <Link
+                className="inline-block hover:text-white transition-all duration-300"
+                target='_blank'
+                href="https://github.com/mlayerprotocol"
+              >
+                <BsGithub size={30} />
               </Link>
         </div>
       </div>
       <div className="flex justify-between">
         <div className="flex flex-col gap-2 lg:mr-[148px]">
-          <span className="text-2xl">ML Stream Explorer</span>
+          <span className="text-2xl">MLayer Stream Studio</span>
           <span className=" font-light   text-opacity-80">by Fero Tech</span>
         </div>
 
         <div className="hidden lg:flex flex-col gap-4">
           <span className="font-semibold mb-2">Company</span>
-          <span className="font-light text-sm  text-opacity-80">About Us</span>
           <span className="font-light text-sm  text-opacity-80">
-            Brand Assets
+          <Link
+               
+                href="https://www.mlayer.network"
+                target='_blank'
+              >
+              www.mlayer.network
+              </Link>
+          </span>
+            
+          <span className="font-light text-sm  text-opacity-80">
+          <Link
+               
+                href="https://mlayer.gitbook.io/white-paper"
+                target='_blank'
+              >
+             Whitepaper
+              </Link>
           </span>
           <span className="font-light text-sm  text-opacity-80">
-            Contact Us
+          <Link
+               
+               href="https://mlayer.gitbook.io/introduction/what-is-mlayer"
+               target='_blank'
+             >
+              Documentation
+              </Link>
           </span>
         </div>
 
         <div className="hidden lg:flex flex-col gap-4">
           <span className="font-semibold mb-2">Socials</span>
+          <Link
+                target='_blank'
+                href="https://github.com/mlayerprotocol" >
           <span className="font-light text-sm  text-opacity-80">
-            Api Documentation
-          </span>
+         Github
+            </span></Link> 
+            <Link
+              
+                href="https://twitter.com/mlayerprotocol"
+                target='_blank'
+              >
           <span className="font-light text-sm  text-opacity-80">
-            Knowledge Base
-          </span>
+           X (formally twitter)
+            </span>
+          </Link>
+          <Link
+            
+                href="https://discord.gg/QazYZYBqus"
+                target='_blank'
+              >
           <span className="font-light text-sm  text-opacity-80">
-            Network Status
-          </span>
+            Discord
+            </span>
+            </Link>
         </div>
 
         <div className="hidden lg:flex flex-col gap-4">
-          <span className="font-semibold mb-2">Products & Services</span>
-          <span className="font-light text-sm  text-opacity-80">Advertise</span>
-          <span className="font-light text-sm  text-opacity-80">
+          {/* <span className="font-semibold mb-2">Products & Services</span> */}
+          {/* <span className="font-light text-sm  text-opacity-80">Advertise</span> */}
+          {/* <span className="font-light text-sm  text-opacity-80">
             Explore-as-a-Service (EaaS)
           </span>
           <span className="font-light text-sm  text-opacity-80">Api Plans</span>
-        </div>
+          */}
+        </div> 
       </div>
     </footer>
   );
